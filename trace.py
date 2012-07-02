42381aa5801bf428b28ceca0a45657feb7af133b
ewentzh <moypray@gmail.com>
2012-07-02 22:30:02 +0800

    Add Trace.py to dump tracegit st

--- trace.py
+++ trace.py
@@ -3,6 +3,7 @@
 ###############################################################
 import sys
 import types
+import time
 ###############################################################
 __author__="ewentzh"
 __date__="$2012-7-1 19:45:36$"
@@ -10,10 +11,10 @@ __date__="$2012-7-1 19:45:36$"
 
 debug_on = "on"
 current_level=1
-debug_info={   1:  "DEBUG",
-               2:  "TRACE",
-               3:  "WRANING",
-               4:  "ERROR"
+debug_info={   1:  "DEBUG   ",
+               2:  "TRACE   ",
+               3:  "WRANING ",
+               4:  "ERROR   "
             }
 
 def set_debug_level(lvl):
@@ -28,15 +29,35 @@ def __debug(info):
     if debug_on == "on":
         sys.stdout.write("[DEBUG]: "+info+"\n")
 
-def print_trace(level,parm):
+def print_trace(level,msg):
     if not debug_info.has_key(level):
         sys.stderr.write("Missing........\n")
         return
     if level < current_level:
         __debug("Current Level:"+str(current_level)+",can not display message!!");
         return
+    formatMsg = header(level,msg)
+    sys.stdout.write(formatMsg)
 
-    sys.stdout.write("["+debug_info[level]+"]:"+parm+"\n")
+
+def header(lvl,msg):
+    file = "test.py"
+    timeNow = time.strftime('%H:%M:%S',time.localtime(time.time()))
+    return "["+debug_info[lvl] + file + " "+timeNow +"]: "+ msg + "\n"
+
+def error(msg):
+    print_trace(4,msg)
+
+def warning(msg):
+    print_trace(3,msg)
+
+def info(msg):
+    print_trace(2,msg)
+
+
+def debug(msg):
+    print_trace(1,msg)
+
 
 
 if __name__ == "__main__":
@@ -46,6 +67,12 @@ if __name__ == "__main__":
     print_trace(2,"MyGod")
     print_trace(3,"aksdfjskladf")
     print_trace(4,"safsadfasdfsadfasdf")
+
+    current_level=0
+    error("Here is The Error!!!!!")
+    warning("Here is the Warning !!!")
+    info("Here is the Info")
+    debug("Here is debug Info")
 
 
 
